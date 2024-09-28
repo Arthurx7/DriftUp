8b38fd07c1dca8674e16b2caea05bc9c364f5e46
YinelaPrimero <yinelaprimero01@gmail.com>
2024-09-27 23:38:04 -0500

    GravityOffBase

---
+++ Assets/Scripts/GravityOff.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityOff : MonoBehaviour
+{
+    private float originalMass;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Car"))
+        {
+            Rigidbody carRigidbody = other.GetComponent<Rigidbody>();
+
+            if (carRigidbody != null)
+            {
+                originalMass = carRigidbody.mass;
+                StartCoroutine(ApplyPowerUpEffects(carRigidbody));
+            }
+        }
+    }
+
+    private IEnumerator ApplyPowerUpEffects(Rigidbody carRigidbody)
+    {
+        carRigidbody.mass = 100f;
+        //carRigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+
+        yield return new WaitForSeconds(1.5f);
+
+        carRigidbody.useGravity = false;
+
+        yield return new WaitForSeconds(10f);
+
+        //carRigidbody.constraints = RigidbodyConstraints.None;
+        carRigidbody.useGravity = true;
+        carRigidbody.mass = originalMass;
+
+    }
+}
